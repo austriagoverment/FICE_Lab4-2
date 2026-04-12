3ee00e787b89cddc542fa588e8e3f7154652a5d2
Vlad D. <vladdidyk1985@gmail.com>
2026-04-12 19:42:54 +0300

    feat: city event class

---
+++ lab4(2)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab4
+{
+    public class CityEvent
+    {
+        private readonly int id;
+        public string title;
+        public string duration;
+        static int counter = 0;
+
+        public CityEvent()
+        {
+            counter++;
+            id = counter;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
